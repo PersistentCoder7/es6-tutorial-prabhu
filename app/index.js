1797aaf4e81e10f566297538447374768f7184f9
Prabhugurudarshan Singh <prabhu25.1975@gmail.com>
2022-01-20 20:16:08 +0530

    Learnt about ES6 let, consts and literal strings

--- app/index.js
+++ app/index.js
@@ -1,8 +1,19 @@
-let a = 'hello';
-console.log(a);
+//const a = 5;
+//a=5-5; //Reassignment is not allowed.
 
-{
-    let a = 'goodbye';
-    console.log('a inside scope', a);
-}
-console.log('a outside scope', a);
+//This kind of update to the object is allowed but the object cannot be reassigned.
+// const array = [1,2,3];
+// array.push(4);
+// console.log(array);
+
+//This is not allowed.
+//array=]1];
+
+// var a = 'hello';
+// var b = 'world';
+// var c = a + ' '+b;
+// console.log(c);
+// let d = `hello ${b}`;
+// console.log(d);
+// let e = `${a} world`;
+// console.log(e);
