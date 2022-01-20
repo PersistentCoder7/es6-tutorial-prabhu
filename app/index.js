dfdc15d78fd17367442abad7b134f233f9043a8d
Prabhugurudarshan Singh <prabhu25.1975@gmail.com>
2022-01-20 21:20:49 +0530

    Explore ES6 spread operator and Destructuring

--- app/index.js
+++ app/index.js
@@ -17,3 +17,61 @@
 // console.log(d);
 // let e = `${a} world`;
 // console.log(e);
+
+//Array spread operator and De-Structuring
+
+// let a = [7,8,9];
+// let b = [6, ...a, 10];
+// console.log(b);
+
+//Sending and array values to individual args
+// function print(a,b,c){
+//     console.log(a,b,c);
+// }
+
+// let z = [1,2,3];
+// print(...z);
+
+//Send individual args and bundling it into an array
+
+// function print(...z){
+//     console.log(z);
+// }
+
+// print(1,3,4);
+
+//Array Destruturing
+// let c = [100, 200];
+// // let a = c[0];
+// // let b = c[1];
+// //Rewritten using destructuring
+// let [a,b]=c;
+// console.log(a,b);
+
+// let fellowship = ["Frodo","Gandalf", "Aragorn"];
+// let [hobbit,wizard,ranger] = fellowship;
+// console.log(hobbit, wizard,ranger);
+
+//Split the array values into different variables.
+// let c = [100,200,300,400,500];
+// let [a, ...b]=c;
+// console.log(a,b);
+
+//Destructuring Objects
+// let wizard = { magical: true, power: 10};
+// // let magical = wizard.magical;
+// // let power = wizard.power;
+// //Alternative way to project it
+// let {magical, power}=wizard;
+// console.log(magical,power);
+
+// //Trick using Destructuring
+// let magical = true;
+// let power = 2;
+
+// let ranger = {magical: false, power:9};
+// //If the variables are already defined use the parenthesis during assignment.
+// ({magical, power} = ranger);
+
+// console.log(magical,power);
+
